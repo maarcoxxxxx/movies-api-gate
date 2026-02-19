23c547abadd4760aa9a9768f848c542524d760ab
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2026-02-19 12:11:34 +0100

    pipeline 01 quality fate

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,10 +17,10 @@ import { MoviesModule } from './movies/movies.module';
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
       autoLoadEntities: true,
-      synchronize: false,
+      synchronize: true,
     }),
 
     MoviesModule,
   ],
 })
-export class AppModule {}
+export class AppModule { }
